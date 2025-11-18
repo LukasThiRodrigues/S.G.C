e1332ccf34f924f32950d37e888e33cbb89afbf5
Lukas Thiago <lukasthi7@gmail.com>
2025-11-18 15:54:44 -0300

    Configuração do karma

---
+++ sgc-app/karma.conf.js
@@ -0,0 +1,39 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client: {
+            clearContext: false
+        },
+        reporters: ['progress', 'kjhtml', 'coverage'],
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage'),
+            subdir: '.',
+            reporters: [
+                { type: 'html' },
+                { type: 'lcov' },
+                { type: 'text-summary' }
+            ]
+        },
+        browsers: ['ChromeHeadlessCI'],
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox',
+                    '--disable-gpu',
+                    '--disable-dev-shm-usage',
+                    '--disable-setuid-sandbox'
+                ]
+            }
+        },
+        singleRun: true,
+    });
+};
