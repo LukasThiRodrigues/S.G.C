82df0a690165f6eddc63d194594417a109ffbbb9
Lukas Thiago <lukasthi7@gmail.com>
2025-11-27 10:45:49 -0300

    Mudança na url da API

--- sgc-app/src/environments/environment.prod.ts
+++ sgc-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://56.124.85.70:3000',
+  apiUrl: 'http://sgc-api-alb-1033747735.sa-east-1.elb.amazonaws.com',
   production: true,
   sentry: {
     dsn: 'https://936533effa446cbb401543dbe9251730@o4510393266012160.ingest.us.sentry.io/4510393270337536'
