f3fc548dc998477c1edba63b287c78dbf712d9c4
Lukas Thiago <lukasthi7@gmail.com>
2025-11-27 14:06:00 -0300

    Configuração do Railway

--- sgc-app/src/environments/environment.prod.ts
+++ sgc-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://sgc-api-alb-1033747735.sa-east-1.elb.amazonaws.com',
+  apiUrl: 'https://sgc-api-production-c0a8.up.railway.app',
   production: true,
   sentry: {
     dsn: 'https://936533effa446cbb401543dbe9251730@o4510393266012160.ingest.us.sentry.io/4510393270337536'
--- sgc-app/src/environments/environment.ts
+++ sgc-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://sgc-api-production-c0a8.up.railway.app',
   sentry: {
     dsn: ''
   }
